08ee9d38e8c83d3762a501da92a79a80bc3f7b3b
Om <omprxz@gmail.com>
2023-11-27 11:59:51 +0530

    Removed ip tracking

--- admin/stats.php
+++ admin/stats.php
@@ -5,7 +5,7 @@ if(isset($_POST['stat'])){
 // If you suspect the user might be behind a proxy or load balancer, check other headers
 if (array_key_exists('HTTP_X_FORWARDED_FOR', $_SERVER)) {
     $ip = $_SERVER['HTTP_X_FORWARDED_FOR'];
-} elseif (array_key_exists('HTTP_CLIENT_IP', $_SERVER)) {
+}elseif (array_key_exists('HTTP_CLIENT_IP', $_SERVER)) {
     $ip = $_SERVER['HTTP_CLIENT_IP'];
 }
 
@@ -13,15 +13,13 @@ if (array_key_exists('HTTP_X_FORWARDED_FOR', $_SERVER)) {
 
     if (!file_exists($statsFilename)) {
         $data = [
-            'page_views' => 0,
-            'ip_addresses' => []
+            'page_views' => 0
         ];
     } else {
         $data = json_decode(file_get_contents($statsFilename), true);
     }
 
     $data['page_views']++;
-    $data['ip_addresses'][] = $ip;
 
     file_put_contents($statsFilename, json_encode($data));
 }
